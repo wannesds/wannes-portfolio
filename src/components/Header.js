b2d8ddc86a28c5ec07fd1c451909cc2c9a5660a6
wannesds <wannesdieltiens@gmail.com>
2020-11-29 19:24:14 +0100

    synch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.scss';
 import Headroom from 'react-headroom';
-import {ReactComponent as WDLogo} from '../SVG/WD-Logo.svg';
+import {ReactComponent as WDLogo} from '../SVG/logo-WD-A.svg';
 
 const Header = () => {
 
