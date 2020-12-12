2c68ecc2527e3dc48c10d6cf2687aa6271a5ed4f
wannesds <wannesdieltiens@gmail.com>
2020-12-12 22:24:08 +0100

    tweaked particles & css

--- src/components/home/Home_link.js
+++ src/components/home/Home_link.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import {Link} from 'react-navigation';
 import './Home_link.scss';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 
