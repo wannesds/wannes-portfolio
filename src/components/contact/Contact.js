29ff31d33c67b1e3043f821f6052c0de5c1b0475
wannesds <wannesdieltiens@gmail.com>
2021-01-19 12:11:06 +0100

    synch

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contact.scss'
+
+const Contact = () => {
+
+    return(
+        <div id="Contact" className="contact-box"> 
+            {/* import svg's and make em links: LinkedIn, Voice, Telegram, DeviantArt, Email, }       
+            <div></div>
+        </div>
+    )
+}
+
+export default Contact;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import Home from '../components/home/Home';
 import About from '../components/about/About';
 import ContentContainer from '../components/content/ContentContainer';
-import Contact from '../components/Contact';
+import Contact from '../components/contact/Contact';
 import Footer from '../components/Footer';
 
 function App() {
