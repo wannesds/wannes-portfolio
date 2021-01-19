45597e642b46be9111ba03606ada4caabc196f2e
wannesds <wannesdieltiens@gmail.com>
2021-01-19 17:04:30 +0100

    sync

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Contact.scss'
+//import {ReactComponent as LinkedinSVG} from '../_assets/linkedin.svg';
 
 const Contact = () => {
 
     return(
         <div id="Contact" className="contact-box"> 
-            {/* import svg's and make em links: LinkedIn, Voice, Telegram, DeviantArt, Email, reddit*/ }       
-            <div></div>
+            {/* import svg's and make em links: LinkedIn, Voice, Telegram, DeviantArt, Email, reddit  
+            <LinkedinSVG className="socialmedia-svg" /> */}
         </div>
     )
 }
